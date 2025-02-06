f438df3d4ef7c5f5cffd8356dc855bbaaa00ece2
Matthieu Renard <m.renard@adacis.net>
2025-02-06 15:48:08 +0100

    Injection SQL #1

---
+++ sql_injection/init_db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+PASSWORD=$(sha256sum <(head -c 128 /dev/urandom) | cut -d' ' -f 1)
+POSTGRES_DB=${POSTGRES_DB-sqlidb}
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE TABLE users (id int primary key, username VARCHAR(255), password VARCHAR(255));
+    INSERT INTO users (id, username, password) VALUES (1, 'admin', '$PASSWORD');
+EOSQL
