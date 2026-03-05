b881406bde815fe9b0a27fa49c087267ffbd64a2
Matthieu Renard <m.renard@adacis.net>
2026-03-05 17:33:10 +0100

    Stack buffer overflow

--- stack_buffer_overflow/program.c
+++ stack_buffer_overflow/program.c
@@ -1,22 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #define NAME_SIZE 50
 
 int win()
 {
-    const char *argv[] = {"/bin/bash", "-i", NULL};
+    const char *argv[] = {"/bin/bash", NULL};
     const char *envp[] = {NULL};
     execve("/bin/bash", argv, envp);
 }
 
 int main(int argc, char *argv[])
 {
+    setreuid(0, 0);
     char name[NAME_SIZE];
 
     printf("Quel est votre nom ?\n");
 
-    scanf("%s", name);
+    scanf("%50s", name);
 
     printf("Bonjour %s\n", name);
 
